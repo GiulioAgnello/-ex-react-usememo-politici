bc1387a891252be4781fc44f74c90c4609c7791d
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-08-23 15:22:37 +0200

    setSerchbar

--- src/components/pages/Posts.jsx
+++ src/components/pages/Posts.jsx
@@ -6,18 +6,33 @@ let baseurlApi = "http://localhost:3333/politicians";
 export default function Posts() {
   // presa dei dati
   const [posts, setPost] = useState([]);
+  const [allPosts, setAllPosts] = useState([]); // aggiungi stato per tutti i post
+  const [text, setText] = useState("");
 
   function fetchpost() {
     axios.get(`${baseurlApi}`).then((res) => {
       console.log(res.data);
 
       setPost(res.data);
+      setAllPosts(res.data); // salva tutti i post
     });
   }
   useEffect(fetchpost, []);
+
+  function handlOnChange(e) {
+    const value = e.target.value;
+    setText(value);
+
+    const filtered = allPosts.filter((post) =>
+      post.name.toLowerCase().includes(value.toLowerCase())
+    );
+    setPost(filtered);
+  }
   return (
     <>
       <div className="container-sm  mt-5">
+        <label htmlFor="search">Cerca</label>
+        <input type="text" value={text} onChange={handlOnChange} />
         <div className="row g-2">
           {posts.map((post) => (
             <div key={post.id} className="col-4  ">
